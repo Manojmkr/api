86ccbb6d9cec061d9ccb18de4ea3afc4f98014a6
unknown <vijaymano14@gmail.com>
2018-05-23 14:09:35 +0530

    My first commit

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'secret': 'Strictly',
+	   'database' : 'mongodb://manojkr:Scorpio@42@ds233500.mlab.com:33500/test1'
+}
+
---
+++ routes/users.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var config = require('../config/database');
+var User = require('../model/user');
+
+
+var app = express();
+var router = express.Router();
+
+
+mongoose.connect(config.database);
+app.set('superSecret', config.secret);
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signup', function (req, res, next) {
+	var userinfo = new User({
+		name: req.body.name,
+		password: req.body.password
+	});
+	userinfo.save(function (err, Asignup) {
+		if (err) return JSON.stringify(err);
+		//saved
+		if (Asignup) {
+
+			res.send({ status: true, message: "success" })
+
+		} else {
+
+			res.send({ status: false, message: "Failure" })
+		}
+	})
+});
+
+module.exports = router;
